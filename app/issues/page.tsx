137dbbadbeb1c155eee4e00f931d5d14444a0715
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-01-09 15:04:16 +0100

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@radix-ui/themes";
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 export default function Issues() {
-  return <div>
-    <Button>New Issue</Button>
-  </div>;
+  return (
+    <div>
+      <Button>
+        <Link href={'issues/new'}>New Issue</Link>
+      </Button>
+    </div>
+  );
 }
