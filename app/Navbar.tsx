008990d9249cc9c3fcc08ac4d6b02a48f995141d
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-01-09 12:17:32 +0100

    Style active link in Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,16 @@
+'use client';
+import clsx from 'clsx';
+import { link } from 'fs';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { IoBugSharp } from 'react-icons/io5';
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: 'Dashboard',
@@ -15,6 +21,7 @@ const Navbar = (props: Props) => {
       href: '/issues',
     },
   ];
+
   return (
     <nav className='flex items-center justify-between text-zinc-500 border-b  px-5 h-14 mb-5'>
       <Link href='/'>
@@ -24,7 +31,10 @@ const Navbar = (props: Props) => {
       <div className='flex gap-x-6'>
         {links.map(({ label, href }) => (
           <Link
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={clsx(
+              'hover:text-zinc-800 transition-colors',
+              href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
+            )}
             href={href}
             key={label}
           >
